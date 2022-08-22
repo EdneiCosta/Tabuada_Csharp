4b31b702f5a5fffa6fe1de58bded6e5ab2f25e74
EdneiCosta <ednei1956@hotmail.com>
2022-08-22 13:24:51 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/ExercicoTabuada.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuada
+{
+
+        internal class ExercicioTabuada
+        {
+            public static void Executar()
+            {
+                Console.WriteLine("Informe um número para a Tabuada");
+                if (int.TryParse(Console.ReadLine(), out int valorUsuario))
+                {
+                    var sb = new StringBuilder();
+                    sb.AppendLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                    sb.AppendLine($"=-=-= TABUADA do {valorUsuario} =-=-=-=-=-=-=-=-=");
+                    sb.AppendLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+
+                    for (int i = 0; i < 11; i++)
+                    {
+                        sb.AppendLine($"{valorUsuario} X {i} = {valorUsuario * i}");
+                    }
+                    Console.WriteLine(sb.ToString());
+                }
+                else
+                {
+                    Console.WriteLine("Numero invalido");
+                }
+            }
+        }
+}
+
+
+
